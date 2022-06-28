5f389ec3b5833bf69be5668aaa9eabff4517ab32
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-06-28 11:00:58 -0700

    Update program 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for num in range(1, 89):
-	if num < 10:
-		print("{:0>2}".format(num), end=", ")
-	else:
-		if str(num) == (str(num)[::-1]) or int(str(num)[::-1]) < num:
-				continue
-		print("{:0>2}".format(num), end=", ")
+    if num < 10:
+        print("{:0>2}".format(num), end=", ")
+    else:
+        if str(num) == (str(num)[::-1]) or int(str(num)[::-1]) < num:
+            continue
+        print("{:0>2}".format(num), end=", ")
 print("{}".format(89))
