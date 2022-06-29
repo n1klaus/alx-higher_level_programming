ffedc86c0a8808d8c7f88f57f45b57ff568d82b2
Nick Nyanjui <nicknyanjui@hotmail.com>
2022-06-29 20:47:55 +0300

    Add 101-compile

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 import sys
+
+
 def print_stderr(str):
-	sys.stderr.write(f"{str}\n")
-	exit(1)
+    sys.stderr.write(f"{str}\n")
+    exit(1)
+
+
 print_stderr("and that piece of art is useful - Dora Korpar, 2015-10-19")
