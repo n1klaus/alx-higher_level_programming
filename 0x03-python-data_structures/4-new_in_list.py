d3767b495186b0d6fdfdc22580d1e86c140b65fd
Nick Nyanjui <nicknyanjui@hotmail.com>
2022-07-01 21:56:02 +0300

    Add 4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """My copied list elements replacing function
+
+    Args:
+        my_list: list of integers
+        idx: index of elements
+        element: new element
+
+    Returns:
+        list with replaced element if True
+        old list if False
+    """
+    new_list = my_list[:]
+    if 0 < idx > len(new_list):
+        return new_list
+    else:
+        new_list[idx] = element
+        return new_list
