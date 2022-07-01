150356a72d16ccb26952229cae27fa36746a0dce
Nick Nyanjui <nicknyanjui@hotmail.com>
2022-07-01 21:06:18 +0300

    Add 1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """My list element retrieving function
+
+    Args:
+        my_list: list of integers
+        idx: index of element
+
+    Returns:
+        index of the list element
+    """
+    return None if 0 < idx > len(my_list) else my_list[idx]
