62d0b00655591c3faa459538377e1ac67b5d23ff
Nick Nyanjui <nicknyanjui@hotmail.com>
2022-07-01 20:37:28 +0300

    Add 0-print_list_integer.py

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """My List printing function
+
+    Args:
+        my_list: list of integers
+
+    Returns:
+        list item
+    """
+    [print("{}".format(my_list[index])) for index in range(len(my_list))]
