ee5ba72aae3510ca2671eb6a2de51b179b14fcc4
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-07-04 11:51:12 -0700

    Update program 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,11 +9,20 @@ def add_tuple(tuple_a=(), tuple_b=()):
     Returns:
         tuple with results from addition
     """
-    if len(tuple_a) >= 2 and len(tuple_a) >= 2:
+    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
         sum1 = tuple_a[0] + tuple_b[0]
         sum2 = tuple_a[1] + tuple_b[1]
-    elif not tuple_a[0] or not tuple_b[0]:
-        sum1 = tuple_a[0] + 0 if not tuple_a[0] else tuple_b[0] + 0
-    elif not tuple_a[1] or not tuple_b[1]:
-        sum2 = tuple_a[1] + 0 if not tuple_a[1] else tuple_b[1] + 0
+    else:
+        if len(tuple_a) == 1:
+            sum1 = tuple_a[0] + tuple_b[0]
+            sum2 = tuple_b[1] + 0
+        elif len(tuple_b) == 1:
+            sum1 = tuple_a[0] + tuple_b[0]
+            sum2 = tuple_a[1] + 0
+        elif len(tuple_a) == 0:
+            sum1 = tuple_b[0] + 0
+            sum2 = tuple_b[1] + 0
+        elif len(tuple_b) == 0:
+            sum1 = tuple_a[0] + 0
+            sum2 = tuple_a[1] + 0
     return sum1, sum2
