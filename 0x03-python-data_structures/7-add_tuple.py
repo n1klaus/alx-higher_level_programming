66a82df86ad75807092af3a02044031d83dc52ed
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-07-04 11:53:31 -0700

    Update program 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -25,4 +25,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         elif len(tuple_b) == 0:
             sum1 = tuple_a[0] + 0
             sum2 = tuple_a[1] + 0
+        else:
+            sum1 += 0
+            sum2 += 0
     return sum1, sum2
