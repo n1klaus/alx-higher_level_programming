9526adf56bec9cb0bb993d20fec7a280527737ff
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-07-05 12:14:17 -0700

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -8,6 +8,6 @@ if inside the main module
 a = 1
 b = 2
 if __name__ == "__main__":
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
 else:
     print("{}".format(""), end="")
