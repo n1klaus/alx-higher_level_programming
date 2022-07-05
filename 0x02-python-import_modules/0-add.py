6af337368ade92ecd5b482c192f5861367b2e6ba
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-07-05 12:18:53 -0700

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-from add_0 import add
 
 """Print addition function
 if inside the main module
 """
 
 if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
