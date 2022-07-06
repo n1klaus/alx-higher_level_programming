5f931184f0747ecad69f6ec4778d25d7c47be071
Nick Nyanjui <nicknyanjui@hotmail.com>
2022-07-07 00:32:13 +0300

    Add 8-simple_delete.py

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    """Function to delete a key in a dictionary.
+
+    Args:
+        a_dictionary: a dictionary of key and value pairs
+        key: key item
+
+    Returns:
+        dictionary with updated values
+    """
+    if (not len(key)):
+        a_dictionary.pop(key)
+    return a_dictionary
