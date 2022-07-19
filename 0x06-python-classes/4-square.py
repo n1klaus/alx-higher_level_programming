5efa228df5dea0229179be869979adb3f5ef42d6
Nick Nyanjui <nicknyanjui@gmail.com>
2022-07-20 00:29:20 +0300

    Add program 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+class Square():
+    """Class definition for a square.
+
+    Args:
+        size (:obj:'int', optional): size of the length
+
+    Attributes:
+        __size (int): size of the length of the new instance
+
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter method to retrieve private attributes of the instance.
+
+        Args:
+            None
+
+        Returns:
+            size of the length of the instance.
+
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method to change private attributes of the instance.
+
+        Args:
+            value (int): size of the length of the new instance
+
+        Returns:
+            size of the length of the instance.
+
+        """
+        try:
+            if value >= 0 and isinstance(value, int):
+                self.size = value
+        except TypeError:
+            print("size must be an integer".format())
+        except ValueError:
+            print("size must be >= 0".format())
+        except Exception:
+            raise
+
+    def area(self):
+        """Method to calculate the area of the square for the instance.
+
+        Args:
+            None
+
+        Returns:
+            area of the square for the instance.
+
+        """
+        return self.__size ** 2
