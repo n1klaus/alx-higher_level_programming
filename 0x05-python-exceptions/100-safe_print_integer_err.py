64f819ab91db23582e936348d2bd5b1b47ca238a
Nick Nyanjui <nicknyanjui@gmail.com>
2022-07-20 20:05:45 +0300

    Add program 100-safe_print_integer_err.py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    """Function to print a value if it is an integer.
+
+    Args:
+        value: any type of value
+
+    Returns:
+        True if value is an integer, and prints it
+        Otherwise False and prints error in stderr
+    """
+    status = 0
+    try:
+        print("{:d}".format(value))
+        status = 1
+    except Exception as e:
+        sys.stderr.write("Exception {}\n".format(e))
+    return status
