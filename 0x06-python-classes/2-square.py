3e757a738df3fe78ed901bd1e772ea4bf9a1bf0d
Nick Nyanjui <nicknyanjui@gmail.com>
2022-07-20 22:43:33 +0300

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -14,40 +14,11 @@ class Square():
         """ The constructor to initialize new square objects
 
         Args:
-            size (int): size of the length
+            size (:obj:'int'): size of the length
 
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
-
-    @property
-    def _size(self):
-        """Getter method to retrieve private attributes of the instance.
-
-        Returns:
-            size of the length of the instance.
-
-        """
-        return self.__size
-
-    @_size.setter
-    def _size(self, size):
-        """Setter method to change private attributes of the instance.
-
-        Args:
-            size (:obj:'int'): size of the length of the new instance
-
-        Returns:
-            int: size of the length of the instance.
-
-        """
-        try:
-            if size >= 0 and isinstance(size, int):
-                self.__size = size
-        except TypeError:
-            print("size must be an integer".format())
-        except ValueError:
-            print("size must be >= 0".format())
-        except Exception:
-            raise
-        finally:
-            return self.__size
