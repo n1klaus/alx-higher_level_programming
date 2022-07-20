3a43e085ac3aa304392df5e0d107808b5d374a56
Nick Nyanjui <nicknyanjui@gmail.com>
2022-07-20 22:10:58 +0300

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
-class Square():
-    """Class definition for a square.
+""" Module to define a square class """
 
-    Args:
-        size: size of the length
+
+class Square():
+    """Class definition for a square of given size
 
     Attributes:
-        __size: size of the length of the new instance
+        __size: size of the length
 
     """
 
     def __init__(self, size):
+        """ The constructor to initialize new square objects
+
+        Args:
+            size: size of the length
+
+        """
         self.__size = size
 
     @property
     def _size(self):
-        """Getter method to retrieve private attributes of the instance.
-
-        Args:
-            None
+        """Getter method to retrieve private attributes of the instance
 
         Returns:
-            size of the length of the instance.
+            size of the length of the instance
 
         """
         return self.__size
