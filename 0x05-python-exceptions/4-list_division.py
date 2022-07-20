2fd290931f5755e78d1a45868732be6a5c4513c0
Nick Nyanjui <nicknyanjui@gmail.com>
2022-07-20 21:31:31 +0300

    Update program 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,21 +11,20 @@ def list_division(my_list_1, my_list_2, list_length):
         new list of length list_length with the values from the division
     """
     new_list = []
-    try:
-        for i in range(len(my_list_1)):
-            if isinstance(my_list_1[i], int) and\
-                    isinstance(my_list_2[i], int):
-                new_list.append(my_list_1[i] / my_list_2[i])
-    except ZeroDivisionError:
-        new_list.append(0)
-        print("division by 0".format())
-    except TypeError:
-        new_list.append(0)
-        print("wrong type".format())
-    except IndexError:
-        new_list.append(0)
-        print("out of range".format())
-    except Exception as e:
-        raise
-    finally:
-        return new_list
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            result = 0
+            print("division by 0".format())
+        except TypeError:
+            result = 0
+            print("wrong type".format())
+        except IndexError:
+            result = 0
+            print("out of range".format())
+        except Exception as e:
+            raise
+        finally:
+            new_list.append(result)
+    return new_list
--- 0x05-python-exceptions/README.md
+++ 0x05-python-exceptions/README.md
@@ -1,1 +1,11 @@
-0-safe_print_list.py
+`0-safe_print_list.py` - Prints x elements of a list\
+`1-safe_print_integer.py` - Prints an integer with `"{:d}".format()`\
+`2-safe_print_list_integers.py` - Prints the first x elements of a list and only integers\
+`3-safe_print_division.py` - Divides 2 integers and prints the result\
+`4-list_division.py` - Divides element by element 2 lists\
+`5-raise_exception.py` - Raises a type exception\
+`6-raise_exception_msg.py` - Raises a name exception with a message\
+`100-safe_print_integer_err.py` - Prints an integer\
+`101-safe_function.py` -  Executes a function safely\
+`102-magic_calculation.py` - Bytecode interpretation\
+`103-python.c` - C functions that print some basic info about Python lists, Python bytes an Python float objects
