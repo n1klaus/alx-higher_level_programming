334b274473409e34641c8c7598d243e262cfcad5
Nick Nyanjui <nicknyanjui@gmail.com>
2022-07-20 22:05:41 +0300

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-""" Module to define Square class """
+""" Module to define an empty square class """
 
 class Square():
-    """Empty Class definition
+    """ Class definition for a square with no arguments
     """
+
     def __init__(self):
+        """ The constructor to initialize new square objects """
         pass
