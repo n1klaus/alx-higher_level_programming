949ef285e051217eb6bf50a383f5d7de4cd1239e
Nick Nyanjui <nicknyanjui@gmail.com>
2022-07-21 23:35:05 +0300

    Add 0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Module to define an adding function """
+
+
+def add_integer(a, b=98):
+    """Function to add two numbers and return the result
+
+    Args:
+        a (:obj:'int'): first variable
+        b (:obj:'int', optional): second variable
+
+    Returns:
+        int: the result of the addition
+
+    """
+    try:
+        if isinstance(a, (int, float)):
+            if isinstance(b, (int, float)):
+                sum = int(a) + int(b)
+            else:
+                raise TypeError("b must be an integer")
+        else:
+            raise TypeError("a must be an integer")
+    except Exception as e:
+        raise
+    return sum
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testfile('tests/0-add_integer.txt')
