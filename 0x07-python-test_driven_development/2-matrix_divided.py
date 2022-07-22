4e047f10512aeef18242d2aa1e4879d1e8198a2d
Nick Nyanjui <nicknyanjui@gmail.com>
2022-07-22 23:17:52 +0300

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,7 +8,7 @@ def matrix_divided(matrix, div):
     Args:
         matrix (:obj:list of lists of 'int' or 'float): 
 			a 2 dimensional list of lists
-        div (:obj:'int' or'float'): variable to divide the matrix elements
+        div (:obj:'int' or 'float'): variable to divide the matrix elements
 
     Returns:
         new matrix with the new results
@@ -22,14 +22,16 @@ def matrix_divided(matrix, div):
         if div == 0:
             raise ZeroDivisionError("division by zero")
         for row in range(len(matrix)):
-            if len(matrix[row]) != len(matrix[row + 1]):
-                raise TypeError("Each row of the matrix must have the same size")
+            if row is not len(matrix) - 1:
+                if len(matrix[row]) != len(matrix[row + 1]):
+                    raise TypeError("Each row of the matrix must have the same size")
             for column in matrix[row]:
                 if isinstance(column, (int, float)):
                     inner_list.append(round((column / div), 2))
                 else:
                     raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            full_matrix.append(inner_list)
+            full_matrix.append(inner_list.copy())
+            inner_list.clear()
         else:
             raise TypeError("a must be an integer")
     except Exception as e:
