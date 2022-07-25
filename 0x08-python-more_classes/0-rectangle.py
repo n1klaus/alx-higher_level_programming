9120b1d341f79befec0b1a7b8f3cb7de1d65a6d3
Nick Nyanjui <nicknyanjui@gmail.com>
2022-07-25 20:57:04 +0300

    Add 0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module to define a rectangle class """
+
+
+class Rectangle():
+    """ Class definition for a rectangle """
+
+    def __init__(self):
+        """ The constructor to initialize new rectangle instances """
+        pass
