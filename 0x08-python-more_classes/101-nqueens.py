95f8e9c2cc87a467cec50e7b05059f5e4229861c
Nick Nyanjui <nicknyanjui@gmail.com>
2022-07-27 18:17:20 +0300

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -2,160 +2,75 @@
 """ Module to define a depth first backtracking solution class """
 
 
+from sys import argv, exit
 class Queens():
-    """ Class definition for solving a NxN queen problem
+    """ Class definition for solving an N queens puzzle on
+    a NxN chessboard
 
     Attributes:
-        __width (int): widht of the rectangle
-        __height (int): height of the rectangle
+        __size (int): number of rows in the chessboard
     """
-    number_of_instances = 0
-    print_symbol = "#"
 
-    def __init__(self, width=0, height=0):
-        """ The constructor to initialize new rectangle instances
+    def __init__(self):
+        """ The constructor to initialize new permutations
 
         Args:
-            width (int, optional): width of the rectangle
-            height (int, optional): height of the Rectangle
+            argv (int): arguments array
         """
-        try:
-            if isinstance(height, int) and height >= 0:
-                self.__height = int(height)
-        except ValueError:
-            print("height must be >= 0".format())
-        except TypeError:
-            print("height must be an integer".format())
-        except Exception:
-            raise
-
-        try:
-            if isinstance(width, int) and width >= 0:
-                self.__width = int(width)
-        except ValueError:
-            print("width must be >= 0".format())
-        except TypeError:
-            print("width must be an integer".format())
-        except Exception:
-            raise
-        Rectangle.number_of_instances += 1
-
-    @property
-    def width(self):
-        """ Getter method to retrieve instance width attribute
-
-        Returns:
-            int: width of the rectangle instance
-        """
-        return self.__width
-
-    @width.setter
-    def width(self, width):
-        """ Setter method to change instance width attribute
-
-        Args:
-            width (int): width of the retangle instance
-        """
-        try:
-            if isinstance(width, int) and width >= 0:
-                self.__width = int(width)
-        except ValueError:
-            print("width must be >= 0".format())
-        except TypeError:
-            print("width must be an integer".format())
-        except Exception:
-            raise
-
-    @property
-    def height(self):
-        """ Getter method to retrieve height attribute
-
-        Returns:
-            int: height of the rectangle instance
-        """
-        return self.__height
-
-    @height.setter
-    def height(self, height):
-        """ Setter method to change the height attribute
-
-        Args:
-            height (int): height of the rectangle instances
-        """
-        try:
-            if isinstance(height, int) and height >= 0:
-                self.__height = int(height)
-        except ValueError:
-            print("height must be >= 0".format())
-        except TypeError:
-            print("height must be an integer".format())
-        except Exception:
-            raise
-
-    def area(self):
-        """ Method to compute the area of the rectangle instance
-
-        Returns:
-            The area of the rectangle"""
-        return self.__width * self.__height
-
-    def perimeter(self):
-        """ Method to compute the perimeter of the rectangle instance
-
-        Returns:
-            The perimeter of the rectangle
-        """
-        perimeter = 0
-        if self.__width != 0 or self.__height != 0:
-            perimeter = 2 * (self.__width + self.__height)
-        return perimeter
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """ Compares the area of two rectangles and returns the largest
-
-        Returns:
-            The largest rectangle which is an instance of Rectangle Class,
-            Otherwise return my_rect_1
-        """
-        if isinstance(rect_1, Rectangle):
-            if isinstance(rect_2, Rectangle):
-                if rect_2.area() > rect_1.area():
-                    return rect_2
+        if len(argv) != 2:
+            print("Usage: nqueens N".format())
+            exit(1)
+        if argv[1].isdigit():
+            if int(argv[1]) >= 4:
+                self.__size = int(argv[1])
             else:
-                raise TypeError("rect_2 must be an instance of Rectangle")
+                print("N must be at least 4".format())
+                exit(1)
         else:
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        return rect_1
+            print("N must be a number".format())
+            exit(1)
 
-    @classmethod
-    def square(cls, size=0):
-        """ Method to create a square instance from a rectangle """
-        if isinstance(size, int):
-            if size > 0:
-                h = w = size
-                return cls(w, h)
-            else:
-                raise ValueError
+    def rows(self):
+        """ Method to return the position of the queen on all the rows """
+        rows = []
+        complete_rows = []
+        if self.__size % 4 != 2 or self.__size % 4 != 3:
+            for index in range(self.__size):
+                if index % 2 == 0:
+                    rows.append(index)
         else:
-            raise TypeError
+            even_row = []
+            odd_row = []
+            for index in range(self.__size):
+                if index % 2 == 0:
+                    even_row.append(index)
+                if index % 2 != 0:
+                    odd_row.append(index)
+            if self.__size % 4 == 2:
+                odd_row.remove(1)
+                odd_row.remove(3)
+                odd_row.insert(0, 1)
+                odd_row.insert(0, 3)
+                odd_row.remove(5)
+                odd_row.append(5)
+                rows = even_row + odd_row
+            if self.__size % 4 == 3:
+                even_row.remove(2)
+                even_row.append(2)
+                odd_row.remove(1)
+                odd_row.remove(3)
+                odd_row.append(1)
+                odd_row.append(3)
+                rows = even_row + odd_row
+        for row_index in range(self.__size):
+            if rows[row_index]:
+                complete_rows.append([row_index, rows[row_index]])
+        return complete_rows
 
-    def __str__(self):
-        """ Print # characters for the size of the rectangle """
-        for h in range(self.__height):
-            for w in range(self.__width):
-                print("{}".format(self.print_symbol), end="")
-            if h != self.__height - 1:
-                print("".format(h))
-        return "".format()
+    def print_row(self):
+        """ Method to print a permuatation for the chessboard of given size """
+        print("{}".format(self.rows()))
 
-    def __repr__(self):
-        """ Print the command representation of creating
-        a similar rectangle instance """
-        return "{}({}, {})".format(self.__class__.__name__,
-                                   self.__width, self.__height)
-
-    def __del__(self):
-        """ Print a message when an instance of the class is deleted """
-        Rectangle.number_of_instances -= 1
-        print("Bye rectangle...".format())
+if __name__ == "__main__":
+    q = Queens()
+    q.print_row()
