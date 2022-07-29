3dbd6bb2ea7d70c2e263204c5f703f4ae4807590
Nick Nyanjui <nicknyanjui@gmail.com>
2022-07-29 19:46:56 +0300

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,23 +6,20 @@ def add_integer(a, b=98):
     """Function to add two numbers and return the result
 
     Args:
-        a (:obj:'int'): first variable
-        b (:obj:'int', optional): second variable
+        a ('int'): first variable
+        b ('int', optional): second variable
 
     Returns:
         int: the result of the addition
 
     """
-    try:
-        if isinstance(a, (int, float)):
-            if isinstance(b, (int, float)):
-                sum = int(a) + int(b)
-            else:
-                raise TypeError("b must be an integer")
+    if isinstance(a, (int, float)):
+        if isinstance(b, (int, float)):
+            sum = int(a) + int(b)
         else:
-            raise TypeError("a must be an integer")
-    except Exception as e:
-        raise
+            raise TypeError("b must be an integer")
+    else:
+        raise TypeError("a must be an integer")
     return sum
 
 
