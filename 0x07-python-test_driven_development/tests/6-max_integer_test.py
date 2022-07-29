10059b3bd9383cf334daff658041cff357f26830
Nick Nyanjui <nicknyanjui@gmail.com>
2022-07-29 20:31:00 +0300

    Update 6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -3,40 +3,54 @@
 
 
 import unittest
+
+
 class TestMaxInteger(unittest.TestCase):
-    """Class definition for testcase on def max_integer(list=[])
+    """Class definition for testcase on 'max_integer(list=[])'
 
     """
     def test_isList(self):
         list1 = []
-        self.assertEqual(isinstance(list1, list), True)
+        self.assertIsInstance(list1, list)
 
     def test_listIsEmpty(self):
         list = []
-        self.assertEqual(len(list) == 0, True)
+        self.assertTrue(len(list) == 0)
+
+    def test_listIsNotEmpty(self):
+        list = [1]
+        self.assertTrue(len(list) > 0)
 
     def test_isIntegerList(self):
         list = [1, 2, 3]
         for i in list:
-            self.assertEqual(isinstance(i, int), True)
+            self.assertIsInstance(i, int)
 
-    def test_isEqual(self):
+    def test_ItemisEqual(self):
         list = [1, 1]
         for index in range(len(list)):
             if index < len(list) - 1 and list[index] == list[index + 1]:
-                self.assertEqual((list[index] - list[index + 1]) == 0, True)
+                self.assertEqual(list[index], list[index + 1])
 
-    def test_isLarger(self):
+    def test_ItemisLarger(self):
         list = [3, 2, 1]
         for index in range(len(list)):
             if index < len(list) - 1 and list[index] > list[index + 1]:
-                self.assertEqual((list[index] - list[index + 1]) > 0, True)
+                self.assertGreater(list[index], list[index + 1])
 
-    def test_isSmaller(self):
+    def test_ItemisSmaller(self):
         list = [1, 2, 3]
         for index in range(len(list)):
             if index < len(list) - 1 and list[index] < list[index + 1]:
-                self.assertEqual((list[index] - list[index + 1]) < 0, True)
+                self.assertLess(list[index], list[index + 1])
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(TestMaxInteger('6-max_integer'))
+    return suite
+
 
 if __name__ == '__main__':
-    unittest.main()
+    runner = unittest.TestRunner()
+    runner.run(suite())
