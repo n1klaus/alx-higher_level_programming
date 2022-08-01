a06655dde9f6c093d4cfb867141793f1e6d784de
Nick Nyanjui <nicknyanjui@gmail.com>
2022-08-01 20:49:42 +0300

    Add 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module to define Geometry class """
+
+
+class BaseGeometry:
+    """ Class definition for geometry calculations """
+
+    def __init__(self):
+        """ Empty constructor for initializing class instances """
+        pass
+
+    def area(self):
+        """ Raise an Exception with the defined custom message """
+        raise Exception("area() is not implemented")
