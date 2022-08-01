c74f517254a0b6aad9ba9cf2a3fa8a497a72ae42
Nick Nyanjui <nicknyanjui@gmail.com>
2022-08-01 18:41:32 +0300

    Update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+""" Module to print object's data and behaviour """
+
+
 def lookup(obj):
+    """ Function to print a python object's attributes and methods
+
+    Args:
+        obj: a python object
+
+    Returns:
+        list: The list of available methods and attributes of the object
+    """
     return dir(obj)
