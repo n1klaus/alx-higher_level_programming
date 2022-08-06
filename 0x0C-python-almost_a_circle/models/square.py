6361e56dcfd280b3819dab0fa5f556d6437d526b
Nick Nyanjui <nicknyanjui@gmail.com>
2022-08-06 19:15:33 +0300

    Update square.py: Add getter and setter methods for size to class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,6 +6,7 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """ Class definition for Square objects """
+
     def __init__(self, size, x=0, y=0, id=None):
         """ Class constructor to instantiate new class instances
 
@@ -17,6 +18,30 @@ class Square(Rectangle):
 
         """
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """ Getter method to access size attribute
+
+        Returns:
+            int: length of the size of the square
+
+        """
+        return self._Rectangle__width
+
+    @size.setter
+    def size(self, value):
+        """ Setter method to modify size attribute
+
+        Args:
+            value (int): new value for the length of the size of the square
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self._Rectangle__width = self._Rectangle__height = value
 
     def __str__(self):
         """ Method to override printable string representation of the instance
