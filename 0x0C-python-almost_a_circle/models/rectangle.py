beac293ff6835af20be3d4122fe72af64f906d60
Nick Nyanjui <nicknyanjui@gmail.com>
2022-08-06 17:00:19 +0300

    Update rectangle.py: Add class setter methods exception handling

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -44,8 +44,16 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        """ Setter method to modify width attribute """
-        assert(isinstance(value, int))
+        """ Setter method to modify width attribute
+
+        Args:
+            value (int): new value for width
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -60,8 +68,16 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        """ Setter method to modify height attribute """
-        assert(isinstance(value, int))
+        """ Setter method to modify height attribute
+
+        Args:
+            value (int): new value for height
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -76,8 +92,16 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        """ Setter method to modify x coordinate attribute """
-        assert(isinstance(value, int))
+        """ Setter method to modify x coordinate attribute
+
+        Args:
+            value (int): new value for x
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -92,6 +116,14 @@ class Rectangle(Base):
 
     @x.setter
     def y(self, value):
-        """ Setter method to modify y coordinate attribute """
-        assert(isinstance(value, int))
+        """ Setter method to modify y coordinate attribute
+
+        Args:
+            value (int): new value for y
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
