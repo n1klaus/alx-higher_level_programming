d3752b324e627c0c0a1b8d8ed72bffd556690903
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-09-08 13:41:44 -0700

    Add 102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,18 +1,7 @@
 -- displays the top 3 of cities temperature during July and August ordered by temperature (descending)
-ALTER TABLE temperatures
-	ADD COLUMN temp1 float
-	ADD COLUMN temp2 float
-	ADD COLUMN avg_temp float AS ((temp1 + temp2) / 2);
-SELECT city, avg_temp
+SELECT city, AVG(value) AS avg_temp
         FROM temperatures
-	SET temp1 =
-	        (SELECT AVG(value)
-		FROM temperatures
-		WHERE month = 7)
-                GROUP BY city
-	SET temp2 =
-		(SELECT AVG(value)
-		FROM temperatures
-		WHERE month = 8
-		GROUP BY city)
-        ORDER BY avg_temp DESC;
+	WHERE month = 7 OR month = 8
+        GROUP BY city
+        ORDER BY avg_temp DESC
+	LIMIT 3;
