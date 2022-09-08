529163ab98de68ac374daeb2db5e0d13da51344b
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-09-08 08:13:38 -0700

    Add 11-genre_id_all_shows.sql

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- lists all shows contained in the database hbtn_0d_tvshows
+SELECT shows.title, genres.genre_id
+	FROM tv_shows AS shows
+	LEFT JOIN tv_show_genres AS genres
+	ON shows.id = genres.show_id
+	ORDER BY shows.title ASC, genres.genre_id ASC;
