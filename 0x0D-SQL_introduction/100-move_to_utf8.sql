2c034d156bf5d9ef0f7369c24071748ccc703f2a
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-09-08 13:43:54 -0700

    Add 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,7 +2,3 @@
 ALTER DATABASE hbtn_0c_0
 	DEFAULT CHARACTER SET utf8mb4
 	COLLATE utf8mb4_unicode_ci;
-USE hbtn_0c_0;
-ALTER TABLE first_table
-        MODIFY name varchar(256)
-	COLLATE utf8mb4_unicode_ci;
