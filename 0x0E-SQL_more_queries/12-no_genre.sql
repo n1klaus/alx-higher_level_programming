cb7cd66ada4583f7dfa600127bc4e167931e31c6
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-09-08 08:27:45 -0700

    Add 12-no_genre.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained in hbtn_0d_tvshows without a genre linked
+SELECT shows.title, genres.genre_id
+	FROM tv_shows AS shows
+	LEFT JOIN tv_show_genres AS genres
+	ON shows.id = genres.show_id
+	WHERE genres.genre_id IS NULL
+	ORDER BY shows.title ASC, genres.genre_id ASC;
