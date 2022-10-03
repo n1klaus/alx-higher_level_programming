061e540097ac23a9a8f02ee754f2e169692b0fd8
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-10-04 00:25:02 +0300

    Add 10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const factorial = (n) => {
+  if (isNaN(n) || n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+};
+
+const f = factorial(parseInt(process.argv[2]));
+console.log(f);
