bc23068f255515a8ea72b103c4d311838faca5d8
Nick Nyanjui <nicknyanjui@gmail.com>
2022-10-05 23:54:46 +0300

    Add 4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let h = 0; h < this.height; h++) {
+      let buffer = '';
+      for (let w = 0; w < this.width; w++) {
+        buffer += 'X';
+      }
+      console.log(buffer);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
