e32bdbc3797399141d9eee9ebdb7b77c39ec4259
Nick Nyanjui <nicknyanjui@gmail.com>
2022-10-05 19:10:04 +0000

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 if (process.argv.length === 2) {
-  process.stderr.write('No argument\n');
+  console.log('No argument\n');
 } else if (process.argv.length === 3) {
-  process.stderr.write('Argument found\n');
+  console.log('Argument found\n');
 } else {
-  process.stderr.write('Arguments found\n');
+  console.log('Arguments found\n');
 }
 process.exit(1);
