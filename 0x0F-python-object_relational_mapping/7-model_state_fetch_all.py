d510db6a19032c23bf02edfa703bfba25b418a4b
Nick Nyanjui <nicknyanjui@gmail.com>
2022-10-08 13:15:51 -0700

    Update 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,14 +15,18 @@ port = 3306
 
 if __name__ == "__main___":
     if len(argv) == 4:
-        username = str(argv[1])
-        passwd = urllib.parse.quote_plus(str(argv[2]))
-        db = str(argv[3])
-        url = f"mysql+mysqldb://{username}:{passwd}@{host}:{port}/{db}"
-        engine = create_engine(url, pool_pre_ping=True)
-        Session = sessionmaker()
-        Session.configure(bind=engine)
-        session = Session()
-        for instance in session.query(State).order_by(State.id).all():
-            stdout.write(f"{instance.id}: {instance.name}")
-        exit()
+        try:
+            username = str(argv[1])
+            passwd = urllib.parse.quote_plus(str(argv[2]))
+            db = str(argv[3])
+            url = f"mysql+mysqldb://{username}:{passwd}@{host}:{port}/{db}"
+            engine = create_engine(url, pool_pre_ping=True)
+            Session = sessionmaker()
+            Session.configure(bind=engine)
+            session = Session()
+            for instance in session.query(State).order_by(State.id).all():
+                stdout.write(f"{instance.id}: {instance.name}\n")
+        except Exception as e:
+            raise
+        finally:
+            exit()
