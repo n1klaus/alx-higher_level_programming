5ffa8fa36176c0759018dd5d81fd94f306eaa6ef
Nick Nyanjui <nicknyanjui@gmail.com>
2022-10-21 13:57:26 -0700

    Add 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #  Takes in a URL, sends a request to that URL,
 #+ and displays the size of the body of the response
-curl -s -w '\n%{size_download}\n' "$1" | tail -n 1
+curl -X GET -s -w '\n%{size_download}\n' "$1" | tail -n 1
