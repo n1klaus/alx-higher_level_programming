28e4a100c46a4e79b004f2bf9b17402fdc08ee44
Nick Nyanjui <nicknyanjui@gmail.com>
2022-10-22 08:55:57 -0700

    Add 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Takes in a letter and sends a POST request
+    to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+from sys import argv
+
+
+def post_url(url, letter):
+    """ Send data to provided URL """
+    try:
+        data = {}
+        data["q"] = letter
+        with requests.post(url, data=data) as resp:
+            json = resp.json()
+            if not json:
+                raise
+            print(f"[{json.get('id')}] {json.get('name')}")
+    except BaseException:
+        if len(json.keys()) == 0:
+            print(f"No result")
+        else:
+            print(f"Not a valid JSON")
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    try:
+        letter = str(argv[1]).strip()
+    except IndexError:
+        letter = ""
+    post_url(url, letter)
