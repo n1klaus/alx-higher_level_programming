ed62ffe993490ebad4e3890629a189813eafac1f
Nick Nyanjui <nicknyanjui@gmail.com>
2022-10-22 07:04:22 -0700

    Add 3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Takes in a URL, sends a request to the URL
+    and displays the body of the response utf-8 decoded
+"""
+from urllib.request import urlopen
+from urllib.error import URLError
+from sys import argv
+
+
+def fetch_url(url):
+    """ Fetches from provided URL """
+    try:
+        with urlopen(url) as resp:
+            html = resp.read()
+            print(f"{str(html, 'utf-8')}")
+    except URLError as e:
+        if hasattr(e, "code"):
+            print(f"Error code: {e.code}")
+
+
+if __name__ == "__main__":
+    url = str(argv[1]).strip()
+    fetch_url(url)
