aeb8087d098f7dcaccf6e006263a181a492670ee
Nick Nyanjui <nicknyanjui@gmail.com>
2022-10-26 02:17:03 -0700

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  Takes in a URL and displays all HTTP methods the server will accept.
-curl -X OPTIONS -i -L -s "$1" | grep -e "Allow" | cut -f 2 -d ":"
+curl -X OPTIONS -i -L -s "$1" | grep -e "Allow" | awk -F':' '{gsub(/^[\t ]+/,"",$2)};{print $2}'
