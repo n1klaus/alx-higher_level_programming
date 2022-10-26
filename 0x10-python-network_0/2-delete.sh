bb9ba71126c5910fd72cf38a52b0b98fb5acadb5
Nick Nyanjui <nicknyanjui@gmail.com>
2022-10-26 01:03:26 -0700

    Update 2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
-#  Sends a DELETE request to the URL passed as the first argument
-#+ and displays the body of the response
+#!/bin/bash
+# Sends a DELETE request to the URL passed as the first argument and displays the body of the response
 (curl -X DELETE -s "$1")
