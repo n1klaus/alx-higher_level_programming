0c8a571e7d9de47930d1e49a84bd20a3ec86da4e
Nick Nyanjui <nicknyanjui@gmail.com>
2022-10-26 00:57:47 -0700

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#  Takes in a URL, sends a request to that URL,
-#+ and displays the size of the body of the response
+#  Takes in a URL, sends a request to that URL, and displays the size of the body of the response
 curl -X GET -s -w '\n%{size_download}\n' "$1" | tail -n 1
