831d7eec1c9b7313fb30e2e2cad8400636b4efd9
Nick Nyanjui <nicknyanjui@gmail.com>
2022-10-26 02:24:13 -0700

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
-#  Takes in a URL, sends a POST request to the passed URL,
-#+ and displays the body of the response
-(curl -L -s -X POST -d "email=test@gmail.com&subject: I will always be here for PLD" "$1")
+#!/bin/bash
+#  Takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -L -s -X POST -d "email=test@gmail.com&subject: I will always be here for PLD" "$1"
