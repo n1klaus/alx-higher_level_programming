954f915a45c4ed872792db1db56a3f33f69ebb27
Nick Nyanjui <nicknyanjui@gmail.com>
2022-10-26 01:01:38 -0700

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-#  Takes in a URL, sends a GET request to that URL,
-#+ and displays the body of the response
-code=$(curl -X GET -L -s -w "\n%{http_code}" "$1" | tail -n 1)
-if [ "$code" == 200 ]
-then
-	curl -X GET -L "$1"
-fi
+#  Takes in a URL, sends a GET request to that URL, and displays the body of the response
+curl -X GET -L -s "$1"
